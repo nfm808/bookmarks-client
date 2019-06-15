572e235e260345e7161620db014794b287a89dd1
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-14 18:43:16 -1000

    finishied routes and added tests

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const uuid = require('uuid/v4')
 const logger = require('../logger')
 const bookmarks  = require('../store')
+const { PORT } = require('../config')
 
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
@@ -17,18 +18,74 @@ bookmarksRouter
     const { name, url, rating=3 } = req.body
 
     if (!name) {
+      logger.error(`name is required`)
       return res
         .status(400)
-        .res('Must include name')
+        .send('Must include name')
     }
 
     if (!url) {
+      logger.error(`url is required`)
       return res
         .status(400)
-        .res('Must include URI')
+        .send('Must include URI')
     }
-    // continue validation here
+    // continue validation here...most likely with 
+    // library
 
+    const id = uuid()
+    const bookmark = {
+      id,
+      name,
+      url,
+      rating
+    }
+
+    bookmarks.push(bookmark)
+    logger.info(`bookmark created with id: ${id} `)
+    res
+      .status(201)
+      .location(`http://localhost:${PORT}/bookmarks/${id}`)
+      .json(id)
+
+  })
+
+bookmarksRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params
+
+    const bookmark = bookmarks.filter(bm => bm.id == id )
+    if ( !id || bookmark.length == 0 ) {
+      logger.error(`Invalid bookmark id: ${id}`)
+      return res
+        .status(404)
+        .send(`${id} is not a valid bookmark id`)
+    }
+
+    return res
+      .status(200)
+      .send(bookmark)
+  })
+  .delete((req, res) => {
+    const { id } = req.params
+
+    const listIndex = bookmarks.findIndex(bm => bm.id == id)
+
+    if ( listIndex === -1 ) {
+      logger.error(`bookmark not found id: ${id}`)
+      return res
+        .status(404)
+        .send('Not Found')
+    }
+
+    bookmarks.splice(listIndex, 1)
+
+    logger.info(`Bookmark Id: ${id} deleted`)
+
+    res
+      .status(204)
+      .end()
   })
 
 module.exports = bookmarksRouter
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,18 @@
 const app = require('../src/app')
+const { API_TOKEN } = process.env
+
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('returns unauthorized without Auth headers', () => {
     return supertest(app)
       .get('/')
+      .expect(401, {"error":"Unauthorized request"})
+  });
+
+  it.only('GET / responds with 200 containing "Hello, world!"', () => {
+    return supertest(app)
+      .get('/')
+      .set({"Authorization": 'Bearer ' + API_TOKEN} )
       .expect(200, 'Hello, world!')
   })
 })
